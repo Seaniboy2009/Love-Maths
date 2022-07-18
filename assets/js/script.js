e24c03557e3f5686e5d726c9936e3df9bbf69727
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-07-18 13:27:57 +0000

    add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,10 @@ function runGame(gameType) {
 
         displaySubtractionQuestion(num1, num2);
 
+    } else if (gameType === "division") {
+
+        displayDivisionQuestion(num1, num2);
+
     } else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -88,6 +92,9 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
 
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+
+        return [operand1 % operand2, "division"];
     } else {
 
         alert(`Unimplimented operator ${operator}`);
@@ -129,5 +136,13 @@ function displayMuiltplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
 
 }
