9072896d1a18ee654f778b3591ddd0e15d98146a
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-07-18 11:55:04 +0000

    add random number generation, add button events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
+// wait for the dom to finish loading before running the game
+// get the button elements and add event listiners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked on ${gameType}`);
+            }
+        })
+        
+    }
+})
+
+/** the main game loop, this runs then the game starts and when the submit button is pressed */
 function runGame() {
+
+    //creates two random numbers when run
+    let num1 = Math.floor(Math.random() * 24) + 1;
+    let num2 = Math.floor(Math.random() * 24) + 1;
 
 }
 
